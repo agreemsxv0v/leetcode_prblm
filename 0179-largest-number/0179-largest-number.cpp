a697abc5b180f2025b624a0556c7a5db1b4653d4
Agreem Saini <102326317+agreemsxv0v@users.noreply.github.com>
2024-09-18 18:35:32 +0530

    Time: 5 ms (70.7%), Space: 17.4 MB (36.02%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string largestNumber(vector<int>& nums) {
+        vector<string> strs;
+        for (int num : nums) {
+            strs.push_back(to_string(num));
+        }
+
+        // Sort the strings based on the custom rule
+        sort(strs.begin(), strs.end(), [](string &a, string &b) {
+            return a + b > b + a;
+        });
+
+        // Edge case: if the largest number is "0", return "0"
+        if (strs[0] == "0") {
+            return "0";
+        }
+
+        // Join all the sorted strings into one large number
+        string ans = "";
+        for (string &s : strs) {
+            ans += s;
+        }
+
+        return ans;
+    }
+};
