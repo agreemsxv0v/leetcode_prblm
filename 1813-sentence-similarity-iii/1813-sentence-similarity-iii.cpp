d6ca976b9c2e654c78aca7bf3ca574e387782502
Agreem Saini <102326317+agreemsxv0v@users.noreply.github.com>
2024-10-06 18:17:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1813-sentence-similarity-iii/1813-sentence-similarity-iii.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool areSentencesSimilar(string sentence1, string sentence2) {
+        if(sentence1.size() < sentence2.size()){
+            return false;
+        
+        }
+
+        else 
+            return true; 
+    }
+};
