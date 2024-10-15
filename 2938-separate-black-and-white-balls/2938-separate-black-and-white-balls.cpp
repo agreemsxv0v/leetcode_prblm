c9bd147b87912cb0ef8fbdbbbb81e423a1ecc431
Agreem Saini <102326317+agreemsxv0v@users.noreply.github.com>
2024-10-15 18:37:14 +0530

    Time: 35 ms (43.16%), Space: 15.5 MB (89.89%) - LeetHub

--- 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.cpp
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.cpp
@@ -2,20 +2,18 @@ class Solution {
 public:
     long long minimumSteps(string s) {
         long long swapball=0;
-
+        int black=0;
         int n=s.length();
 
         for(int i=0;i<n;i++)
         {
-            int j=i+1;
 
-            if(s[i] == '1' && s[j]=='0')
+            if(s[i]=='0')
             {
-                swap(s[i],s[j]);
-                swapball++;
+                swapball += (long long) black;
             }
             else{
-                continue;
+                black++;
             }
         }
         return swapball;
