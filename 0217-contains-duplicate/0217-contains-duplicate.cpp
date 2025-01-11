7f6b768fa67499d02a46645eef70a331d726d68e
Agreem Saini <102326317+agreemsxv0v@users.noreply.github.com>
2025-01-11 19:12:36 +0530

    Time: 75 ms (6.93%), Space: 77.4 MB (6.77%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        set<int> st;
+        for(int i=0;i<nums.size();i++){
+            st.insert(nums[i]);
+        }
+
+        if(nums.size() != st.size())
+            return true;
+        return false;
+    }
+};
